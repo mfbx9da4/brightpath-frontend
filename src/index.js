8038c1d8c73f93645a4968f1bb0062de40416a01
David Adler <dalberto.adler@gmail.com>
2019-10-11 12:31:33 +0100

    add sw

--- src/index.js
+++ src/index.js
@@ -131,3 +131,16 @@ class Root extends Component {
 }
 
 render(<Root></Root>, document.querySelector("#react-root"))
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./sw-test/sw.js", { scope: "./sw-test/" })
+    .then(reg => {
+      // registration worked
+      console.log("Registration succeeded. Scope is " + reg.scope)
+    })
+    .catch(error => {
+      // registration failed
+      console.log("Registration failed with " + error)
+    })
+}
